c33661c6e45ae1c6b88bfe06e938e60020804c06
Hongqiang Shan <hongqis@amazon.com>
2021-06-08 23:09:39 +0800

    use hash map cache

--- flink-formats/flink-avro-glue-schema-registry/src/main/java/org/apache/flink/formats/avro/glue/schema/registry/GlueSchemaRegistryInputStreamDeserializer.java
+++ flink-formats/flink-avro-glue-schema-registry/src/main/java/org/apache/flink/formats/avro/glue/schema/registry/GlueSchemaRegistryInputStreamDeserializer.java
@@ -18,32 +18,28 @@
 
 package org.apache.flink.formats.avro.glue.schema.registry;
 
+import org.apache.avro.SchemaParseException;
+
 import org.apache.flink.formats.avro.utils.MutableByteArrayInputStream;
 
 import com.amazonaws.services.schemaregistry.deserializers.AWSDeserializer;
 import com.amazonaws.services.schemaregistry.exception.AWSSchemaRegistryException;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.util.concurrent.UncheckedExecutionException;
-import lombok.NonNull;
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Parser;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 /**
  * AWS Glue Schema Registry input stream de-serializer to accept input stream and extract schema
  * from it and remove schema registry information in the input stream.
  */
 public class GlueSchemaRegistryInputStreamDeserializer {
-    private static final int CACHE_SIZE = 200;
     private final AWSDeserializer awsDeserializer;
-    private final LoadingCache<String, Schema> cache;
+    private final Map<String, Schema> cache;
 
     /**
      * Constructor accepts configuration map for AWS Deserializer.
@@ -56,12 +52,12 @@ public class GlueSchemaRegistryInputStreamDeserializer {
                         .credentialProvider(DefaultCredentialsProvider.builder().build())
                         .configs(configs)
                         .build();
-        cache = initCache(CACHE_SIZE);
+        cache = new HashMap<>();
     }
 
     public GlueSchemaRegistryInputStreamDeserializer(AWSDeserializer awsDeserializer) {
         this.awsDeserializer = awsDeserializer;
-        cache = initCache(CACHE_SIZE);
+        cache = new HashMap<>();
     }
 
     /**
@@ -83,27 +79,20 @@ public class GlueSchemaRegistryInputStreamDeserializer {
         byte[] deserializedBytes = awsDeserializer.getActualData(inputBytes);
         mutableByteArrayInputStream.setBuffer(deserializedBytes);
         Schema schema;
-        try {
+        if (cache.containsKey(schemaDefinition)) {
             schema = cache.get(schemaDefinition);
-        } catch (ExecutionException | UncheckedExecutionException e) {
-            String message =
-                    "Error occurred while parsing schema, see inner exception for details.";
-            throw new AWSSchemaRegistryException(message, e);
+        } else {
+            try {
+                Parser schemaParser = new Schema.Parser();
+                schema = schemaParser.parse(schemaDefinition);
+                cache.put(schemaDefinition, schema);
+            } catch (SchemaParseException e) {
+                String message =
+                        "Error occurred while parsing schema, see inner exception for details.";
+                throw new AWSSchemaRegistryException(message, e);
+            }
         }
 
         return schema;
-    }
-
-    private LoadingCache<String, Schema> initCache(int cacheSize) {
-        final CacheLoader<String, Schema> loader = new CacheLoader<>() {
-            @Override
-            public Schema load(@NonNull String schemaDefinition) {
-                Parser schemaParser = new Schema.Parser();
-                return schemaParser.parse(schemaDefinition);
-            }
-        };
-        return CacheBuilder.newBuilder()
-                .maximumSize(cacheSize)
-                .build(loader);
     }
 }
